e0fb0d1ffa36d27116a62aedea5dc4fc496be9c2
wangwenlin <wangwenlin@innotechx.com>
2021-04-14 16:25:20 +0800

    添加 初始化文件

---
+++ htmlStaticServer/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("/wwl-code/youxi/"));
+app.get("*", function (req, res) {
+	res.sendFile(path.resolve(__dirname, "./xzbdbj/index.html"));
+});
+const server = app.listen(7001, function () {
+	const host = server.address().address;
+	const port = server.address().port;
+
+	console.log("Example app listening at http://%s:%s", host, port);
+});
